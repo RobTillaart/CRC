e84849ae129a875b16065994b7f05f2c7b769edf
rob tillaart <rob.tillaart@gmail.com>
2021-03-31 20:30:52 +0200

    fix reverseIN flag

---
+++ test/unit_test_crc16.cpp
@@ -0,0 +1,85 @@
+//
+//    FILE: unit_test_001.cpp
+//  AUTHOR: Rob Tillaart
+//    DATE: 2021-01-01
+// PURPOSE: unit tests for the CRC library
+//          https://github.com/RobTillaart/CRC
+//          https://github.com/Arduino-CI/arduino_ci/blob/master/REFERENCE.md
+//
+
+// supported assertions
+// ----------------------------
+// assertEqual(expected, actual);               // a == b
+// assertNotEqual(unwanted, actual);            // a != b
+// assertComparativeEquivalent(expected, actual);    // abs(a - b) == 0 or (!(a > b) && !(a < b))
+// assertComparativeNotEquivalent(unwanted, actual); // abs(a - b) > 0  or ((a > b) || (a < b))
+// assertLess(upperBound, actual);              // a < b
+// assertMore(lowerBound, actual);              // a > b
+// assertLessOrEqual(upperBound, actual);       // a <= b
+// assertMoreOrEqual(lowerBound, actual);       // a >= b
+// assertTrue(actual);
+// assertFalse(actual);
+// assertNull(actual);
+
+// // special cases for floats
+// assertEqualFloat(expected, actual, epsilon);    // fabs(a - b) <= epsilon
+// assertNotEqualFloat(unwanted, actual, epsilon); // fabs(a - b) >= epsilon
+// assertInfinity(actual);                         // isinf(a)
+// assertNotInfinity(actual);                      // !isinf(a)
+// assertNAN(arg);                                 // isnan(a)
+// assertNotNAN(arg);                              // !isnan(a)
+
+
+#include <ArduinoUnitTests.h>
+
+
+#include "Arduino.h"
+#include "CRC16.h"
+
+
+char str[24] = "123456789";
+uint8_t * data = (uint8_t *) str;
+
+
+unittest_setup()
+{
+}
+
+unittest_teardown()
+{
+}
+
+
+unittest(test_crc16)
+{
+  fprintf(stderr, "VERSION: %s\n", CRC_LIB_VERSION);
+
+  assertEqual(0x29B1, crc16(data, 9, 0x1021, 0xFFFF, 0x0000, false, false ));
+  assertEqual(0xBB3D, crc16(data, 9, 0x8005, 0x0000, 0x0000, true,  true  ));
+  assertEqual(0xE5CC, crc16(data, 9, 0x1021, 0x1D0F, 0x0000, false, false ));
+  assertEqual(0xFEE8, crc16(data, 9, 0x8005, 0x0000, 0x0000, false, false ));
+  assertEqual(0x4C06, crc16(data, 9, 0xC867, 0xFFFF, 0x0000, false, false ));
+  assertEqual(0x9ECF, crc16(data, 9, 0x8005, 0x800D, 0x0000, false, false ));
+  assertEqual(0x007E, crc16(data, 9, 0x0589, 0x0000, 0x0001, false, false ));
+  assertEqual(0x007F, crc16(data, 9, 0x0589, 0x0000, 0x0000, false, false ));
+  assertEqual(0xEA82, crc16(data, 9, 0x3D65, 0x0000, 0xFFFF, true,  true  ));
+  assertEqual(0xC2B7, crc16(data, 9, 0x3D65, 0x0000, 0xFFFF, false, false ));
+  assertEqual(0xD64E, crc16(data, 9, 0x1021, 0xFFFF, 0xFFFF, false, false ));
+  assertEqual(0x44C2, crc16(data, 9, 0x8005, 0x0000, 0xFFFF, true,  true  ));
+  assertEqual(0x6F91, crc16(data, 9, 0x1021, 0xFFFF, 0x0000, true,  true  ));
+  assertEqual(0x63D0, crc16(data, 9, 0x1021, 0xB2AA, 0x0000, true,  true  ));
+  assertEqual(0xD0DB, crc16(data, 9, 0x8BB7, 0x0000, 0x0000, false, false ));
+  assertEqual(0x0FB3, crc16(data, 9, 0xA097, 0x0000, 0x0000, false, false ));
+  assertEqual(0x26B1, crc16(data, 9, 0x1021, 0x89EC, 0x0000, true,  true  ));
+  assertEqual(0xB4C8, crc16(data, 9, 0x8005, 0xFFFF, 0xFFFF, true,  true  ));
+  assertEqual(0xBF05, crc16(data, 9, 0x1021, 0xC6C6, 0x0000, true,  true  ));
+  assertEqual(0x2189, crc16(data, 9, 0x1021, 0x0000, 0x0000, true,  true  ));
+  assertEqual(0x4B37, crc16(data, 9, 0x8005, 0xFFFF, 0x0000, true,  true  ));
+  assertEqual(0x906E, crc16(data, 9, 0x1021, 0xFFFF, 0xFFFF, true,  true  ));
+  assertEqual(0x31C3, crc16(data, 9, 0x1021, 0x0000, 0x0000, false, false ));
+}
+
+
+unittest_main()
+
+// --------
---
+++ test/unit_test_crc64.cpp
@@ -0,0 +1,64 @@
+//
+//    FILE: unit_test_001.cpp
+//  AUTHOR: Rob Tillaart
+//    DATE: 2021-01-01
+// PURPOSE: unit tests for the CRC library
+//          https://github.com/RobTillaart/CRC
+//          https://github.com/Arduino-CI/arduino_ci/blob/master/REFERENCE.md
+//
+
+// supported assertions
+// ----------------------------
+// assertEqual(expected, actual);               // a == b
+// assertNotEqual(unwanted, actual);            // a != b
+// assertComparativeEquivalent(expected, actual);    // abs(a - b) == 0 or (!(a > b) && !(a < b))
+// assertComparativeNotEquivalent(unwanted, actual); // abs(a - b) > 0  or ((a > b) || (a < b))
+// assertLess(upperBound, actual);              // a < b
+// assertMore(lowerBound, actual);              // a > b
+// assertLessOrEqual(upperBound, actual);       // a <= b
+// assertMoreOrEqual(lowerBound, actual);       // a >= b
+// assertTrue(actual);
+// assertFalse(actual);
+// assertNull(actual);
+
+// // special cases for floats
+// assertEqualFloat(expected, actual, epsilon);    // fabs(a - b) <= epsilon
+// assertNotEqualFloat(unwanted, actual, epsilon); // fabs(a - b) >= epsilon
+// assertInfinity(actual);                         // isinf(a)
+// assertNotInfinity(actual);                      // !isinf(a)
+// assertNAN(arg);                                 // isnan(a)
+// assertNotNAN(arg);                              // !isnan(a)
+
+
+#include <ArduinoUnitTests.h>
+
+
+#include "Arduino.h"
+#include "CRC64.h"
+
+
+char str[24] = "123456789";
+uint8_t * data = (uint8_t *) str;
+
+
+unittest_setup()
+{
+}
+
+unittest_teardown()
+{
+}
+
+
+unittest(test_crc64)
+{
+  fprintf(stderr, "VERSION: %s\n", CRC_LIB_VERSION);
+
+  fprintf(stderr, "no reference yet\n");
+  assertEqual(1, 1);
+}
+
+
+unittest_main()
+
+// --------
