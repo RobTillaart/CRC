3bd8f07994abc8efad884356094a0ea853d2f0a6
tsiselsk <wlodzimierz.ciesielski@nokia.com>
2023-05-08 15:43:44 +0200

    Added yield functions

--- src/CRC.cpp
+++ src/CRC.cpp
@@ -10,8 +10,8 @@ uint8_t crc8(const uint8_t *array, size_t length,
 }
 
 uint16_t crc12(const uint8_t *array, size_t length,
-              const uint16_t polynome, const uint16_t initial, const uint16_t xorOut,
-              const bool reverseIn, const bool reverseOut)
+               const uint16_t polynome, const uint16_t initial, const uint16_t xorOut,
+               const bool reverseIn, const bool reverseOut)
 {
   CRC12 crc(polynome, initial, xorOut, reverseIn, reverseOut);
   crc.add(array, length);
@@ -19,8 +19,8 @@ uint16_t crc12(const uint8_t *array, size_t length,
 }
 
 uint16_t crc16(const uint8_t *array, size_t length,
-              const uint16_t polynome, const uint16_t initial, const uint16_t xorOut,
-              const bool reverseIn, const bool reverseOut)
+               const uint16_t polynome, const uint16_t initial, const uint16_t xorOut,
+               const bool reverseIn, const bool reverseOut)
 {
   CRC16 crc(polynome, initial, xorOut, reverseIn, reverseOut);
   crc.add(array, length);
@@ -28,8 +28,8 @@ uint16_t crc16(const uint8_t *array, size_t length,
 }
 
 uint32_t crc32(const uint8_t *array, size_t length,
-              const uint32_t polynome, const uint32_t initial, const uint32_t xorOut,
-              const bool reverseIn, const bool reverseOut)
+               const uint32_t polynome, const uint32_t initial, const uint32_t xorOut,
+               const bool reverseIn, const bool reverseOut)
 {
   CRC32 crc(polynome, initial, xorOut, reverseIn, reverseOut);
   crc.add(array, length);
@@ -37,10 +37,55 @@ uint32_t crc32(const uint8_t *array, size_t length,
 }
 
 uint64_t crc64(const uint8_t *array, size_t length,
-              const uint64_t polynome, const uint64_t initial, const uint64_t xorOut,
-              const bool reverseIn, const bool reverseOut)
+               const uint64_t polynome, const uint64_t initial, const uint64_t xorOut,
+               const bool reverseIn, const bool reverseOut)
 {
   CRC64 crc(polynome, initial, xorOut, reverseIn, reverseOut);
   crc.add(array, length);
+  return crc.getCRC();
+}
+
+uint8_t yieldCrc8(const uint8_t *array, size_t length,
+                  const uint8_t polynome, const uint8_t initial, const uint8_t xorOut,
+                  const bool reverseIn, const bool reverseOut)
+{
+  CRC8 crc(polynome, initial, xorOut, reverseIn, reverseOut);
+  crc.yieldAdd(array, length);
+  return crc.getCRC();
+}
+
+uint16_t yieldCrc12(const uint8_t *array, size_t length,
+                    const uint16_t polynome, const uint16_t initial, const uint16_t xorOut,
+                    const bool reverseIn, const bool reverseOut)
+{
+  CRC12 crc(polynome, initial, xorOut, reverseIn, reverseOut);
+  crc.yieldAdd(array, length);
+  return crc.getCRC();
+}
+
+uint16_t yieldCrc16(const uint8_t *array, size_t length,
+                    const uint16_t polynome, const uint16_t initial, const uint16_t xorOut,
+                    const bool reverseIn, const bool reverseOut)
+{
+  CRC16 crc(polynome, initial, xorOut, reverseIn, reverseOut);
+  crc.yieldAdd(array, length);
+  return crc.getCRC();
+}
+
+uint32_t yieldCrc32(const uint8_t *array, size_t length,
+                    const uint32_t polynome, const uint32_t initial, const uint32_t xorOut,
+                    const bool reverseIn, const bool reverseOut)
+{
+  CRC32 crc(polynome, initial, xorOut, reverseIn, reverseOut);
+  crc.yieldAdd(array, length);
+  return crc.getCRC();
+}
+
+uint64_t yieldCrc64(const uint8_t *array, size_t length,
+                    const uint64_t polynome, const uint64_t initial, const uint64_t xorOut,
+                    const bool reverseIn, const bool reverseOut)
+{
+  CRC64 crc(polynome, initial, xorOut, reverseIn, reverseOut);
+  crc.yieldAdd(array, length);
   return crc.getCRC();
 }
--- src/CRC.h
+++ src/CRC.h
@@ -18,37 +18,82 @@
 
 #define CRC_LIB_VERSION       (F("0.3.3"))
 
-uint8_t crc8(const uint8_t *array, size_t length,
-             const uint8_t polynome = CRC8_POLYNOME,
-             const uint8_t initial  = CRC8_INITIAL,
-             const uint8_t xorOut   = CRC8_XOR_OUT,
-             const bool reverseIn   = CRC8_REF_IN,
-             const bool reverseOut  = CRC8_REF_OUT);
+uint8_t crc8(
+    const uint8_t *array, size_t length,
+    const uint8_t polynome = CRC8_POLYNOME,
+    const uint8_t initial  = CRC8_INITIAL,
+    const uint8_t xorOut   = CRC8_XOR_OUT,
+    const bool reverseIn   = CRC8_REF_IN,
+    const bool reverseOut  = CRC8_REF_OUT);
 
-uint16_t crc12(const uint8_t *array, size_t length,
-               const uint16_t polynome = CRC12_POLYNOME,
-               const uint16_t initial  = CRC12_INITIAL,
-               const uint16_t xorOut   = CRC12_XOR_OUT,
-               const bool reverseIn    = CRC12_REF_IN,
-               const bool reverseOut   = CRC12_REF_OUT);
+uint16_t crc12(
+    const uint8_t *array, size_t length,
+    const uint16_t polynome = CRC12_POLYNOME,
+    const uint16_t initial  = CRC12_INITIAL,
+    const uint16_t xorOut   = CRC12_XOR_OUT,
+    const bool reverseIn    = CRC12_REF_IN,
+    const bool reverseOut   = CRC12_REF_OUT);
 
-uint16_t crc16(const uint8_t *array, size_t length,
-               const uint16_t polynome = CRC16_POLYNOME,
-               const uint16_t initial  = CRC16_INITIAL,
-               const uint16_t xorOut   = CRC16_XOR_OUT,
-               const bool reverseIn    = CRC16_REF_IN,
-               const bool reverseOut   = CRC16_REF_OUT);
+uint16_t crc16(
+    const uint8_t *array, size_t length,
+    const uint16_t polynome = CRC16_POLYNOME,
+    const uint16_t initial  = CRC16_INITIAL,
+    const uint16_t xorOut   = CRC16_XOR_OUT,
+    const bool reverseIn    = CRC16_REF_IN,
+    const bool reverseOut   = CRC16_REF_OUT);
 
-uint32_t crc32(const uint8_t *array, size_t length,
-               const uint32_t polynome = CRC32_POLYNOME,
-               const uint32_t initial  = CRC32_INITIAL,
-               const uint32_t xorOut   = CRC32_XOR_OUT,
-               const bool reverseIn    = CRC32_REF_IN,
-               const bool reverseOut   = CRC32_REF_OUT);
+uint32_t crc32(
+    const uint8_t *array, size_t length,
+    const uint32_t polynome = CRC32_POLYNOME,
+    const uint32_t initial  = CRC32_INITIAL,
+    const uint32_t xorOut   = CRC32_XOR_OUT,
+    const bool reverseIn    = CRC32_REF_IN,
+    const bool reverseOut   = CRC32_REF_OUT);
 
-uint64_t crc64(const uint8_t *array, size_t length,
-               const uint64_t polynome = CRC64_POLYNOME,
-               const uint64_t initial  = CRC64_INITIAL,
-               const uint64_t xorOut   = CRC64_XOR_OUT,
-               const bool reverseIn    = CRC64_REF_IN,
-               const bool reverseOut   = CRC64_REF_OUT);
+uint64_t crc64(
+    const uint8_t *array, size_t length,
+    const uint64_t polynome = CRC64_POLYNOME,
+    const uint64_t initial  = CRC64_INITIAL,
+    const uint64_t xorOut   = CRC64_XOR_OUT,
+    const bool reverseIn    = CRC64_REF_IN,
+    const bool reverseOut   = CRC64_REF_OUT);
+
+uint8_t yieldCrc8(
+    const uint8_t *array, size_t length,
+    const uint8_t polynome = CRC8_POLYNOME,
+    const uint8_t initial  = CRC8_INITIAL,
+    const uint8_t xorOut   = CRC8_XOR_OUT,
+    const bool reverseIn   = CRC8_REF_IN,
+    const bool reverseOut  = CRC8_REF_OUT);
+
+uint16_t yieldCrc12(
+    const uint8_t *array, size_t length,
+    const uint16_t polynome = CRC12_POLYNOME,
+    const uint16_t initial  = CRC12_INITIAL,
+    const uint16_t xorOut   = CRC12_XOR_OUT,
+    const bool reverseIn    = CRC12_REF_IN,
+    const bool reverseOut   = CRC12_REF_OUT);
+
+uint16_t yieldCrc16(
+    const uint8_t *array, size_t length,
+    const uint16_t polynome = CRC16_POLYNOME,
+    const uint16_t initial  = CRC16_INITIAL,
+    const uint16_t xorOut   = CRC16_XOR_OUT,
+    const bool reverseIn    = CRC16_REF_IN,
+    const bool reverseOut   = CRC16_REF_OUT);
+
+uint32_t yieldCrc32(
+    const uint8_t *array, size_t length,
+    const uint32_t polynome = CRC32_POLYNOME,
+    const uint32_t initial  = CRC32_INITIAL,
+    const uint32_t xorOut   = CRC32_XOR_OUT,
+    const bool reverseIn    = CRC32_REF_IN,
+    const bool reverseOut   = CRC32_REF_OUT);
+
+uint64_t yieldCrc64(
+    const uint8_t *array, size_t length,
+    const uint64_t polynome = CRC64_POLYNOME,
+    const uint64_t initial  = CRC64_INITIAL,
+    const uint64_t xorOut   = CRC64_XOR_OUT,
+    const bool reverseIn    = CRC64_REF_IN,
+    const bool reverseOut   = CRC64_REF_OUT);
