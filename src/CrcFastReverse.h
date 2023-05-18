1fdc1b2927318c4538023357964cb7ca6eeb214b
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2023-05-18 09:55:04 +0200

    Newer deprecated form

--- src/CrcFastReverse.h
+++ src/CrcFastReverse.h
@@ -7,8 +7,8 @@ uint16_t reverse12bits(uint16_t in);
 uint32_t reverse32bits(uint32_t in);
 uint64_t reverse64bits(uint64_t in);
 
-__attribute__((deprecated)) uint8_t reverse8(uint8_t in);
-__attribute__((deprecated)) uint16_t reverse16(uint16_t in);
-__attribute__((deprecated)) uint16_t reverse12(uint16_t in);
-__attribute__((deprecated)) uint32_t reverse32(uint32_t in);
-__attribute__((deprecated)) uint64_t reverse64(uint64_t in);
+[[deprecated("Use reverse8bits")]] uint8_t reverse8(uint8_t in);
+[[deprecated("Use reverse12bits")]] uint16_t reverse16(uint16_t in);
+[[deprecated("Use reverse16bits")]] uint16_t reverse12(uint16_t in);
+[[deprecated("Use reverse32bits")]] uint32_t reverse32(uint32_t in);
+[[deprecated("Use reverse64bits")]] uint64_t reverse64(uint64_t in);
