f280b80785dca0d8d4aef21b4d5d7c12d9d548ff
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2023-05-18 09:49:10 +0200

    Added old deprecated reverse functions

--- src/CrcFastReverse.cpp
+++ src/CrcFastReverse.cpp
@@ -46,3 +46,29 @@ uint64_t reverse64bits(uint64_t in)
   x = (x >> 32) | (x << 32);
   return x;
 }
+
+
+uint8_t reverse8(uint8_t in)
+{
+  return reverse8bits(in);
+}
+
+uint16_t reverse12(uint16_t in)
+{
+  return reverse12bits(in);
+}
+
+uint16_t reverse16(uint16_t in)
+{
+  return reverse16bits(in);
+}
+
+uint32_t reverse32(uint32_t in)
+{
+  return reverse32bits(in);
+}
+
+uint64_t reverse64(uint64_t in)
+{
+  return reverse64bits(in);
+}
--- src/CrcFastReverse.h
+++ src/CrcFastReverse.h
@@ -6,3 +6,9 @@ uint16_t reverse16bits(uint16_t in);
 uint16_t reverse12bits(uint16_t in);
 uint32_t reverse32bits(uint32_t in);
 uint64_t reverse64bits(uint64_t in);
+
+__attribute__((deprecated)) uint8_t reverse8(uint8_t in);
+__attribute__((deprecated)) uint16_t reverse16(uint16_t in);
+__attribute__((deprecated)) uint16_t reverse12(uint16_t in);
+__attribute__((deprecated)) uint32_t reverse32(uint32_t in);
+__attribute__((deprecated)) uint64_t reverse64(uint64_t in);
