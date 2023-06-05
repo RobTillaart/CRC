f576849a0640fcd3c394e77d5ad8fb365675ab65
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2023-06-05 16:12:50 +0200

    Alligned header of CRC.h

--- src/CRC.h
+++ src/CRC.h
@@ -2,7 +2,7 @@
 //
 //    FILE: CRC.h
 //  AUTHOR: Rob Tillaart
-// VERSION: 0.3.3
+// VERSION: 1.0.0
 // PURPOSE: Arduino library for CRC8, CRC12, CRC16, CRC32, CRC64
 //     URL: https://github.com/RobTillaart/CRC
 //
